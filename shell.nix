7cfa2fcebe18c48ff515c27e4c03527e4226f838
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-08-24 15:47:35 -0600

    feat: init commit

---
+++ shell.nix
@@ -0,0 +1,31 @@
+{ pkgs ? (
+    let
+      inherit (builtins) fetchTree fromJSON readFile;
+      inherit ((fromJSON (readFile ./flake.lock)).nodes) nixpkgs gomod2nix;
+    in
+    import (fetchTree nixpkgs.locked) {
+      overlays = [
+        (import "${fetchTree gomod2nix.locked}/overlay.nix")
+      ];
+    }
+  )
+}:
+let
+  goEnv = pkgs.mkGoEnv { pwd = ./.; };
+in
+pkgs.mkShell {
+  name = "gogal";
+  packages = with pkgs; [
+    goEnv
+    gomod2nix
+    gopls
+    gotools
+    go-tools
+  ];
+
+  # enter zsh on startup
+  shellHook = ''
+    zsh
+    exit
+  '';
+}
