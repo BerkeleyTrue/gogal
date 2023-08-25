636f95888f20c637126d65ab9e6c5d2ab358e8fe
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-08-25 01:42:07 -0600

    feat: add pug engine

--- internal/app/app.go
+++ internal/app/app.go
@@ -6,10 +6,15 @@ import (
 	"berkeleytrue/gogal/internal/infra"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/template/pug/v2"
 )
 
 func Run(cfg *config.Config) {
-	app := fiber.New()
+  engine := pug.New("./web/views", ".pug")
+
+	app := fiber.New(fiber.Config{
+    Views: engine,
+  })
 
 	handlers.Public(app)
 
--- internal/app/controllers/main.go
+++ internal/app/controllers/main.go
@@ -3,5 +3,5 @@ package controllers
 import "github.com/gofiber/fiber/v2"
 
 func Index(c *fiber.Ctx) error {
-	return c.SendString("Hello, World 👋!")
+	return c.Render("index", fiber.Map{})
 }
