f65de52c3198ce6536de83eeb57a7db85211372e
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-08-28 23:48:14 -0600

    feat: add pics file

--- internal/app/controllers/pics.go
+++ internal/app/controllers/pics.go
@@ -1,25 +1,32 @@
 package controllers
 
 import (
-	"berkeleytrue/gogal/internal/utils"
-	"fmt"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
+
+	"berkeleytrue/gogal/internal/utils"
 )
 
-
 func (s *Service) Pics(c *fiber.Ctx) error {
-  pics := c.Params("dir")
-  dir := s.directory + "/" + pics + "/"
-  dirs := utils.GetDirectores(dir)
+	pics := c.Params("dir")
+  picsSlice := strings.Split(pics, "/")
+  curUri := "/pics"
+  bcSize := len(picsSlice) + 1
+  breadcrumbs := make([]struct{ Name string; Uri string}, bcSize)
+  breadcrumbs[0] = struct{ Name string; Uri string}{Name: "home", Uri: "/"}
 
-  fmt.Println(s.directory)
-  fmt.Println(pics);
-  fmt.Println(dir)
+  for i, bc := range strings.Split(pics, "/") {
+    curUri += "/" + bc
+    breadcrumbs[i + 1] = struct{Name string; Uri string}{Name: bc, Uri: curUri}
+  }
 
-  return c.Render("index", fiber.Map{
-    "Title":     "Hello, World!",
-    "Directory": s.directory,
-    "Dirs":      dirs,
-  }, "layouts/main")
+	dir := s.directory + "/" + pics + "/"
+	dirs := utils.GetDirectores(dir)
+
+	return c.Render("pics", fiber.Map{
+		"Title":       pics,
+		"Dirs":        dirs,
+		"BreadCrumbs": breadcrumbs,
+	}, "layouts/main")
 }
