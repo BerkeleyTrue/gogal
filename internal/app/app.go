f8885c67183c6ab9bfc43bca22c41e6ba9eec347
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-09-03 01:37:55 -0600

    feat(public): embed public dirs

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,25 +1,33 @@
 package app
 
 import (
+	"net/http"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
 	"go.uber.org/fx"
 
 	"berkeleytrue/gogal/config"
 	"berkeleytrue/gogal/internal/app/controllers"
 	"berkeleytrue/gogal/internal/app/services"
+	"berkeleytrue/gogal/web/public"
 )
 
 var Module = fx.Options(
   fx.Provide(services.NewImageService),
   fx.Provide(services.NewGalleryService),
 	fx.Provide(controllers.NewController),
-	fx.Invoke(SetupStatic),
 	fx.Invoke(controllers.RegisterRoutes),
+	fx.Invoke(SetupStatic),
 )
 
 func SetupStatic(app *fiber.App, cfg *config.Config) {
 	directory := cfg.Directory
 
 	app.Static("/images", directory)
-	app.Static("/", "./web/public")
+
+	app.Use("/", filesystem.New(filesystem.Config{
+    Root: http.FS(public.PublicFS),
+    Browse: false,
+  }))
 }
---
+++ web/public/embed.go
@@ -0,0 +1,6 @@
+package public
+
+import "embed"
+
+//go:embed *
+var PublicFS embed.FS
