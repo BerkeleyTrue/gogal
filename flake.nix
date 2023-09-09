9d76ac303f8a097926633d8be4fe2c1689087a3e
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-09-08 19:32:38 -0600

    fix(nix): convert shell to flake-parts module

--- flake.nix
+++ flake.nix
@@ -16,11 +16,13 @@
 
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        (import ./shell.nix)
+      ];
       systems = ["x86_64-linux"];
       perSystem = {pkgs, ...}: {
         formatter = pkgs.alejandra;
         packages.default = pkgs.callPackage ./. {inherit (inputs) nix-filter;};
-        devShells.default = pkgs.callPackage ./shell.nix {};
       };
     };
 }
--- shell.nix
+++ shell.nix
@@ -1,61 +1,63 @@
-{ pkgs }:
-let
-  # update the vendorSha256 of the default package
-  update-vendor-sha = pkgs.writeShellScriptBin "update-vendor-sha" ''
-    set -exuo pipefail
+{...}: {
+  perSystem = {pkgs, ...}: let
+    # update the vendorSha256 of the default package
+    update-vendor-sha = pkgs.writeShellScriptBin "update-vendor-sha" ''
+      set -exuo pipefail
 
-    failedbuild=$(nix build --impure 2>&1 || true)
-    # echo "$failedbuild"
-    checksum=$(echo "$failedbuild" | awk '/got:.*sha256/ { print $2 }')
-    echo -n "\n\nchecksum: $checksum"
-    # do nothing if no checksum was found
-    if [ -z "$checksum" ]; then
-      exit 0
-    fi
-    sed -i -e "s|vendorSha256 = \".*\"|vendorSha256 = \"$checksum\"|" ./default.nix
-  '';
+      failedbuild=$(nix build --impure 2>&1 || true)
+      # echo "$failedbuild"
+      checksum=$(echo "$failedbuild" | awk '/got:.*sha256/ { print $2 }')
+      echo -n "\n\nchecksum: $checksum"
+      # do nothing if no checksum was found
+      if [ -z "$checksum" ]; then
+        exit 0
+      fi
+      sed -i -e "s|vendorSha256 = \".*\"|vendorSha256 = \"$checksum\"|" ./default.nix
+    '';
 
-  watch-compile = pkgs.writeShellScriptBin "watch-compile" ''
-    ${pkgs.concurrently}/bin/concurrently -n css,go \
-      "${pkgs.nodejs_18}/bin/npx tailwindcss -i ./web/input.css -o ./web/public/css/output.css --watch"\
-      "${pkgs.air}/bin/air"
-  '';
+    watch-compile = pkgs.writeShellScriptBin "watch-compile" ''
+      ${pkgs.concurrently}/bin/concurrently -n css,go \
+        "${pkgs.nodejs_18}/bin/npx tailwindcss -i ./web/input.css -o ./web/public/css/output.css --watch"\
+        "${pkgs.air}/bin/air"
+    '';
 
-  watch-tests = pkgs.writeShellScriptBin "watch-tests" ''
-    ${pkgs.ginkgo}/bin/ginkgo watch -r -p
-  '';
+    watch-tests = pkgs.writeShellScriptBin "watch-tests" ''
+      ${pkgs.ginkgo}/bin/ginkgo watch -r -p
+    '';
 
-  tailwindcss-language-server = pkgs.writeShellScriptBin "tailwindcss-language-server" ''
-    ${pkgs.nodejs_18}/bin/npx @tailwindcss/language-server --stdio
-  '';
-in
-pkgs.mkShell {
-  name = "gogal";
-  packages = with pkgs; [
-    go
-    gopls # language server
-    gotools
-    go-tools
-    air # live reload
-    ginkgo # testing framework
+    tailwindcss-language-server = pkgs.writeShellScriptBin "tailwindcss-language-server" ''
+      ${pkgs.nodejs_18}/bin/npx @tailwindcss/language-server --stdio
+    '';
+  in {
+    devShells.default = pkgs.mkShell {
+      name = "gogal";
+      packages = with pkgs; [
+        go
+        gopls # language server
+        gotools
+        go-tools
+        air # live reload
+        ginkgo # testing framework
 
-    # editor
-    nodePackages.vscode-langservers-extracted # html/css language server
-    nodePackages.typescript-language-server # typescript language server
+        # editor
+        nodePackages.vscode-langservers-extracted # html/css language server
+        nodePackages.typescript-language-server # typescript language server
 
-    # css stuff
-    nodejs_18
+        # css stuff
+        nodejs_18
 
-    # scripts
-    update-vendor-sha
-    watch-compile
-    watch-tests
-    tailwindcss-language-server
-  ];
+        # scripts
+        update-vendor-sha
+        watch-compile
+        watch-tests
+        tailwindcss-language-server
+      ];
 
-  # enter zsh on startup
-  shellHook = ''
-    zsh
-    exit
-  '';
+      # enter zsh on startup
+      shellHook = ''
+        zsh
+        exit
+      '';
+    };
+  };
 }
