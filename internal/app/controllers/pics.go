5a91840767485bb7807d6ffa26d8793692bd6b28
Berkeley True <berkeleytrue@users.noreply.github.com>
2023-09-10 22:04:58 -0600

    feat(image-view): add range fetch image

--- internal/app/controllers/pics.go
+++ internal/app/controllers/pics.go
@@ -1,6 +1,10 @@
 package controllers
 
 import (
+	"path/filepath"
+	"strconv"
+	"strings"
+
 	"github.com/gofiber/fiber/v2"
 
 	"berkeleytrue/gogal/internal/utils"
@@ -8,6 +12,8 @@ import (
 
 func (c *Controller) Pics(ctx *fiber.Ctx) error {
 	isHx := ctx.Get("HX-Request") == "true"
+  isRangeRequest := ctx.Get("HX-Trigger") == "range";
+
 	uri := ctx.Params("*")
 	breadcrumbs := buildBreadcrumbs(uri)
 
@@ -15,10 +21,39 @@ func (c *Controller) Pics(ctx *fiber.Ctx) error {
 	isDir := c.imageService.IsPathADir(path)
 	dirs := utils.GetDirectories(path, c.directory)
 
-	if !isDir {
+	if isRangeRequest || !isDir {
+
+    if isRangeRequest {
+      thisImageIndexQuery, err := strconv.Atoi(ctx.Query("index", "0"))
+
+      if err != nil {
+        return err
+      }
+
+      thisImage, err := utils.GetAtIndex(dirs, thisImageIndexQuery - 1);
+
+      if err != nil {
+        return err
+      }
+
+      uri = uri + "/" + thisImage.Name
+      path = c.directory + "/" + uri
+
+      ctx.Set("HX-Push-Url", "/pics/" + uri)
+    }
+
 		thisImageIndex, numOfPics, nextDir, prevDir := c.imageService.GetImages(
 			dirs,
 			uri,
+		)
+
+    breadcrumbs = buildBreadcrumbs(uri)
+
+		dirname := strings.Replace(
+			filepath.Dir(path),
+			c.directory,
+			"",
+			1,
 		)
 
 		return ctx.Render("index", fiber.Map{
@@ -33,6 +68,7 @@ func (c *Controller) Pics(ctx *fiber.Ctx) error {
 			"NextImage":   nextDir.Image,
 			"PrevImage":   prevDir.Image,
 			"IsHx":        isHx,
+			"Dirname":     "/pics" + dirname,
 		}, "layouts/main")
 	}
 
